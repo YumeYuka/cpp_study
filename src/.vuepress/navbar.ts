c310602ea3d4e4c485bbafa9db65d84a8d64c84a
YumeYuka <yumeyuka@oom-wg.dev>
2025-07-10 23:09:34 +0800

    initialize project structure with basic configuration and styles

---
+++ src/.vuepress/navbar.ts
@@ -0,0 +1,11 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/",
+  "/guide/",
+  {
+    text: "Github",
+    icon: "book",
+    link: "https://github/yumeyuka/cpp_study",
+  },
+]);
---
+++ src/.vuepress/sidebar.ts
@@ -0,0 +1,14 @@
+import { sidebar } from "vuepress-theme-hope";
+
+export default sidebar({
+  "/": [
+    "",
+    {
+      text: "指南",
+      icon: "laptop-code",
+      prefix: "guide/",
+      link: "guide/",
+      children: "structure",
+    },
+  ],
+});
